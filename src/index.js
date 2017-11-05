87207cb3baf1cff664a9d5ab89cad4d2f76f40fa
imonQUE <robquincey@Tom-Quinceys-MacBook-Pro.local>
2017-11-05 12:12:33 +0000

    Removed ball website.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill'
-import CanvasMap from './canvas-map'
-
-let map=CanvasMap({
-  textContainer:document.querySelector('.text'),
-  mapSrc:'img/map.svg',
-  trailVisitedColor:'#A232FF',
-  fontPresentColor:'#A232FF',
-}).appendTo('.container')
